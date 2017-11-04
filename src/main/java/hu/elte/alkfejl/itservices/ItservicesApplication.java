f2d906ccb7e2f5962136fadda6f4cf0843902040
Vukman György <gyurivukman@gmail.com>
2017-11-04 11:36:48 +0100

    fixing wrong structure of starting app, part 2

---
+++ src/main/java/hu/elte/alkfejl/itservices/ItservicesApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.alkfejl.itservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItservicesApplication.class, args);
+	}
+}
