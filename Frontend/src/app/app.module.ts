44839a1da0e0a9dcd0a2446b55f75f3ea526765d
Simon Tamás <simon.tamas.mark@gmail.com>
2017-11-15 20:35:06 +0100

    Created Frontend Angular project, added a basic login page

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AuthenticationModule } from './/authentication.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AuthenticationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
